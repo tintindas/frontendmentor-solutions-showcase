aa25ac17727481e25022b44e9e19183d9d812d8a
Upamanyu Das <upamanyu.das.work@gmail.com>
2020-04-23 13:12:07 +0530

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './data/data'
+import Header from './components/Header';
+import Card from './components/Card'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <div className='grid'>
+        {data.map((item, index) =>
+          <Card key={index} item = {item} />
+        )}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+
+const Heading = () => {
+    return(
+        <div className='header'>
+            <h1>Frontend Mentor Showcase</h1>
+            <p>Solutions by <a href='https://github.com/tintindas' rel='noopener nofollow noreferrer' target='_blank'>tintindas</a></p>
+        </div>
+    )
+    
+}
+
+export default Heading;
