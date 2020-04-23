ddd6782634558a3a1ca4c4248bd965d129263b43
Upamanyu Das <upamanyu.das.work@gmail.com>
2020-04-23 13:13:59 +0530

    Updates

---
+++ precache-manifest.0a03edd47398f9709c697c6eec3224df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e059dd3b506f3f31bf553a7d8ef3285",
+    "url": "/frontendmentor-solutions-showcase/index.html"
+  },
+  {
+    "revision": "42083c1686d5a0f0748a",
+    "url": "/frontendmentor-solutions-showcase/static/css/main.906796a2.chunk.css"
+  },
+  {
+    "revision": "705d9b7a2f0142e0ab59",
+    "url": "/frontendmentor-solutions-showcase/static/js/2.bdcb9fc6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/frontendmentor-solutions-showcase/static/js/2.bdcb9fc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42083c1686d5a0f0748a",
+    "url": "/frontendmentor-solutions-showcase/static/js/main.afda5f5b.chunk.js"
+  },
+  {
+    "revision": "639fbda416be0789421c",
+    "url": "/frontendmentor-solutions-showcase/static/js/runtime-main.6fbb0efb.js"
+  }
+]);
