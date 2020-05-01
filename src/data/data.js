950987164eca3b98af35159c5f36955fc7890fd7
Upamanyu Das <upamanyu.das.work@gmail.com>
2020-05-01 13:29:19 +0530

    add social media dashboard with theme switcher

--- src/data/data.js
+++ src/data/data.js
@@ -1,51 +1,66 @@
-const data = [{
-        name: "Fylo Landing Page with Two Column Layout",
-        repo: "https://github.com/tintindas/fylo-landing-page-with-two-column-layout",
-        demo: "https://tintindas.github.io/fylo-landing-page-with-two-column-layout/",
-        date: "18-04-2020"
-    },
-    {
-        name: "Huddle Landing Page with Single Introductory Section",
-        repo: "https://github.com/tintindas/huddle-landing-page-with-single-introductory-section",
-        demo: "https://tintindas.github.io/huddle-landing-page-with-single-introductory-section/",
-        date: "16-04-2020"
-    },
-    {
-        name: "Huddle Landing Page with Alternating Feature Blocks",
-        repo: "https://github.com/tintindas/huddle-landing-page-with-alternating-feature-blocks",
-        demo: "https://tintindas.github.io/huddle-landing-page-with-alternating-feature-blocks",
-        date: "15-04-2020"
-    },
-    {
-        name: "Ping Coming Soon Page",
-        repo: "https://github.com/tintindas/ping-coming-soon-page",
-        demo: "https://tintindas.github.io/ping-coming-soon-page/",
-        date: "14-04-2020"
-    },
-    {
-        name: "Single Price Grid Component",
-        repo: "https://github.com/tintindas/single-price-grid-component",
-        demo: "https://tintindas.github.io/single-price-grid-component/",
-        date: "13-04-2020"
-    },
-    {
-        name: "Intro Component with Signup Form",
-        repo: "https://github.com/tintindas/intro-component-with-signup-form",
-        demo: "https://tintindas.github.io/intro-component-with-signup-form/",
-        date: "12-04-2020"
-    },
-    {
-        name: "Base Apparel Coming Soon Page",
-        repo: "https://github.com/tintindas/base-apparel-coming-soon",
-        demo: "https://tintindas.github.io/base-apparel-coming-soon/",
-        date: "11-04-2020"
-    },
-    {
-        name: "Four Card Feature Section",
-        repo: "https://github.com/tintindas/four-card-feature-section",
-        demo: "https://tintindas.github.io/four-card-feature-section/",
-        date: "07-03-2020"
-    }
-]
+const data = [
+  {
+    name: 'Social Media Dashboard with Theme Switcher',
+    repo:
+      'https://github.com/tintindas/social-media-dashboard-with-theme-switcher',
+    demo:
+      'https://tintindas.github.io/social-media-dashboard-with-theme-switcher/',
+    date: '1-05-2020',
+  },
+  {
+    name: 'Fylo Landing Page with Two Column Layout',
+    repo:
+      'https://github.com/tintindas/fylo-landing-page-with-two-column-layout',
+    demo:
+      'https://tintindas.github.io/fylo-landing-page-with-two-column-layout/',
+    date: '18-04-2020',
+  },
+  {
+    name: 'Huddle Landing Page with Single Introductory Section',
+    repo:
+      'https://github.com/tintindas/huddle-landing-page-with-single-introductory-section',
+    demo:
+      'https://tintindas.github.io/huddle-landing-page-with-single-introductory-section/',
+    date: '16-04-2020',
+  },
+  {
+    name: 'Huddle Landing Page with Alternating Feature Blocks',
+    repo:
+      'https://github.com/tintindas/huddle-landing-page-with-alternating-feature-blocks',
+    demo:
+      'https://tintindas.github.io/huddle-landing-page-with-alternating-feature-blocks',
+    date: '15-04-2020',
+  },
+  {
+    name: 'Ping Coming Soon Page',
+    repo: 'https://github.com/tintindas/ping-coming-soon-page',
+    demo: 'https://tintindas.github.io/ping-coming-soon-page/',
+    date: '14-04-2020',
+  },
+  {
+    name: 'Single Price Grid Component',
+    repo: 'https://github.com/tintindas/single-price-grid-component',
+    demo: 'https://tintindas.github.io/single-price-grid-component/',
+    date: '13-04-2020',
+  },
+  {
+    name: 'Intro Component with Signup Form',
+    repo: 'https://github.com/tintindas/intro-component-with-signup-form',
+    demo: 'https://tintindas.github.io/intro-component-with-signup-form/',
+    date: '12-04-2020',
+  },
+  {
+    name: 'Base Apparel Coming Soon Page',
+    repo: 'https://github.com/tintindas/base-apparel-coming-soon',
+    demo: 'https://tintindas.github.io/base-apparel-coming-soon/',
+    date: '11-04-2020',
+  },
+  {
+    name: 'Four Card Feature Section',
+    repo: 'https://github.com/tintindas/four-card-feature-section',
+    demo: 'https://tintindas.github.io/four-card-feature-section/',
+    date: '07-03-2020',
+  },
+];
 
 export default data;
